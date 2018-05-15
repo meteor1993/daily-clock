118d1ba8f0dfb779dbce36c446717ea5baf2ae39
weishiyao <inwsy@hotmail.com>
2018-05-15 16:16:05 +0800

    add first task

---
+++ daily-clock-task/src/main/java/com/springcloud/task/system/utils/Constant.java
@@ -0,0 +1,78 @@
+package com.springboot.dailyclock.system.utils;
+
+/**
+ * Created by weishiyao on 2018/1/9.
+ * 静态常量类
+ */
+public class Constant {
+
+    // 微信浏览器
+    public static final String WECHAT_BROWSER = "MicroMessenger";
+
+    // 微信用户信息
+    public static final String WX_MP_USER = "WX_MP_USER";
+
+    // 微信openid
+    public static final String WX_MP_OPENID = "WX_MP_OPENID";
+
+    // 微信小程序openid
+    public static final String WX_MINIAPP_OPENID = "WX_MINIAPP_OPENID";
+
+    // 微信小程序用户信息
+    public static final String WX_MINIAPP_USER = "WX_MINIAPP_USER";
+
+    // 原始访问地址
+    public static final String PREVIOUS_URL = "PREVIOUS_URL";
+
+    // 后端用户登录信息
+    public static final String ADMIN_USER_INFO = "ADMIN_USER_INFO";
+
+    // 返回json错误代码
+    public static final String JSON_ERROR_CODE = "0";
+
+    // 返回json成功代码
+    public static final String JSON_SUCCESS_CODE = "1";
+
+    // 当前用户未登录
+    public static final String ADMIN_NO_LOGIN = "-1";
+
+    // 全局成功代码
+    public static final String SUCCESS_CODE = "1";
+
+    // 全局失败代码
+    public static final String FAIL_CODE = "0";
+
+    // 缓存需打卡列表盘口0
+    public static final String TODAY_NEED_SIGN_USER_0 = "TODAY_NEED_SIGN_USER_0";
+
+    // 缓存打卡记录列表key盘口0
+    public static final String TODAY_SIGN_USER_LOG_0 = "TODAY_SIGN_USER_LOG_0";
+
+    // 缓存需打卡列表盘口1
+    public static final String TODAY_NEED_SIGN_USER_1 = "TODAY_NEED_SIGN_USER_1";
+
+    // 缓存打卡记录列表key盘口1
+    public static final String TODAY_SIGN_USER_LOG_1 = "TODAY_SIGN_USER_LOG_1";
+
+    // 缓存需打卡列表盘口2
+    public static final String TODAY_NEED_SIGN_USER_2 = "TODAY_NEED_SIGN_USER_2";
+
+    // 缓存打卡记录列表key盘口2
+    public static final String TODAY_SIGN_USER_LOG_2 = "TODAY_SIGN_USER_LOG_2";
+
+    // 缓存需打卡列表盘口3
+    public static final String TODAY_NEED_SIGN_USER_3 = "TODAY_NEED_SIGN_USER_3";
+
+    // 缓存打卡记录列表key盘口3
+    public static final String TODAY_SIGN_USER_LOG_3 = "TODAY_SIGN_USER_LOG_3";
+
+    // 未打卡标志位
+    public static final String CLOCK_TYPE_0 = "0";
+
+    // 已打卡标志位
+    public static final String CLOCK_TYPE_1 = "1";
+
+    // 补打卡标志位
+    public static final String CLOCK_TYPE_2 = "2";
+
+}
